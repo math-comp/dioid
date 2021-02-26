dad1cbe523a689d5057eb69807cab76012312277
Pierre Roux <pierre@roux01.fr>
2021-02-26 21:42:52 +0100

    Reimplement kleene and residuation theory

--- complete_dioid.v
+++ complete_dioid.v
@@ -8,6 +8,10 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
+Reserved Notation "x ^+" (at level 20, format "x ^+").
+Reserved Notation "x ^*" (at level 20, format "x ^*").
+
+Local Open Scope classical_set_scope.
 Local Open Scope order_scope.
 Local Open Scope dioid_scope.
 
@@ -16,9 +20,9 @@ Import Order.Theory.
 HB.mixin Record CompleteDioid_of_Dioid_and_CompleteLattice D
          of Dioid D & CompleteLattice D := {
   set_mulDl : forall (a : D) (B : set D),
-      a * set_join B = set_join [set a * x | x in B]%classic;
+      a * set_join B = set_join [set a * x | x in B];
   set_mulDr : forall (a : D) (B : set D),
-      set_join B * a = set_join [set x * a | x in B]%classic;
+      set_join B * a = set_join [set x * a | x in B];
 }.
 
 HB.structure Definition CompleteDioid :=
@@ -74,7 +78,10 @@ Proof. by move=> x; rewrite -set_addDl setUT. Qed.
 Lemma add_topd : left_zero (top D) add.
 Proof. by move=> x; rewrite adddC add_dtop. Qed.
 
-Lemma set_add_O (F : nat -> D) : set_add [set F i | i in 'I_1] = F O.
+Lemma set_add_0 (F : nat -> D) : set_add [set F i | i in 'I_0] = 0.
+Proof. by rewrite -bottom_zero; apply: f_equal; rewrite -subset0 => x [[]]. Qed.
+
+Lemma set_add_1 (F : nat -> D) : set_add [set F i | i in 'I_1] = F O.
 Proof.
 rewrite -(addd0 (F O)) -bottom_zero -set_addDl setU0; apply: f_equal.
 by rewrite predeqP => x; split=> [[y] | ->]; [rewrite ord1|exists ord0].
@@ -117,6 +124,271 @@ move=> H; apply: set_add_lim_nat => k.
 exact/(le_trans (H k))/set_add_led.
 Qed.
 
+Section KleeneOperatorsDefinitions.
+
+Definition exp a n := iterop n *%D a 1.
+Definition op_kleene a := set_add [set of exp a].
+Definition op_plus a := set_add [set of fun i => exp a i.+1].
+Definition div a b := set_add [set c | c * b <= a].
+
+End KleeneOperatorsDefinitions.
+
+Local Notation "a ^ i" := (exp a i) : dioid_scope.
+Local Notation "a ^*" := (op_kleene a) (at level 20) : dioid_scope.
+Local Notation "a ^+" := (op_plus a) (at level 20) : dioid_scope.
+Local Notation "a / b" := (div a b) : dioid_scope.
+
+Section KleeneStar.
+
+Lemma exp0 a : a ^ 0 = 1.
+Proof. by []. Qed.
+
+Lemma expS i a : a ^ (i.+1) = a * (a ^ i).
+Proof. by case: i => //; rewrite exp0 muld1. Qed.
+
+Lemma expSr i a : a ^ i.+1 = a ^ i * a.
+Proof.
+elim: i => [ | i Hi]; first by rewrite exp0 mul1d.
+by rewrite expS {1}Hi expS muldA.
+Qed.
+
+Lemma kleeneSr a : a^* = 1 + a^+.
+Proof.
+rewrite -(exp0 a) -set_addDl /op_kleene.
+apply: f_equal; rewrite predeqP => b; split.
+- by move=> [[| i] _ <-]; [left|right; exists i].
+- by move=> [-> | [i _ <-]]; [exists O|exists i.+1].
+Qed.
+
+Lemma plusSr a : a ^+ = a * a ^*.
+Proof.
+rewrite set_mulDl /op_plus /op_kleene.
+apply: f_equal; rewrite predeqP => b; split.
+- by move=> [i _ <-]; rewrite expS; exists (a ^ i); [exists i|].
+- by move=> [c [i _ <-] <-]; rewrite -expS; exists i.
+Qed.
+
+Lemma plus_le_kleene a : a^+ <= a^*.
+Proof. by rewrite le_def kleeneSr adddCA adddd. Qed.
+
+Lemma le_plus a : a <= a ^+.
+Proof. by rewrite le_def plusSr kleeneSr muldDr muld1 adddA adddd. Qed.
+
+Lemma le_kleene a : a <= a^*.
+Proof. exact/(le_trans (le_plus a))/plus_le_kleene. Qed.
+
+Lemma kleene_mul_expl a n : a ^ n * a ^* <= a^*.
+Proof.
+elim: n => [|n IHn]; [by rewrite mul1d|].
+rewrite expSr -muldA.
+move: IHn; apply/le_trans/led_mul2l.
+rewrite -plusSr; apply: plus_le_kleene.
+Qed.
+
+Lemma kleene_monotony a b : a <= b -> a ^* <= b ^*.
+Proof.
+move=> Hab.
+apply: set_add_led_set => k.
+have Hyp : forall i, a ^ i <= b ^ i.
+  elim=> [|i IHi].
+  - by rewrite exp0.
+  - by rewrite !expS; apply: led_mul.
+elim: k => [|k IHk].
+- by rewrite set_add_0 le0d.
+- rewrite !set_add_S; exact: led_add.
+Qed.
+
+Lemma kleene_1r a : (a + 1) ^* = a ^*.
+Proof.
+apply/le_anti/andP; split; [|exact/kleene_monotony/led_addr].
+suff Hyp : forall i, (a + 1) ^ i = set_add [set a ^ j | j in 'I_i.+1].
+{ apply: set_add_led_set => k.
+  elim: k => [ | k IHk]; [by rewrite set_add_0 le0d|].
+  rewrite !set_add_S Hyp.
+  rewrite -[in X in _ <= X](adddd (set_add _)) -adddA.
+  apply: (led_add IHk).
+  by rewrite set_add_S. }
+elim=> [ | i IHi]; [by rewrite set_add_1 !exp0|].
+rewrite expS muldDl mul1d IHi set_mulDl.
+set B' := [set a ^ j | j in [set j | (1 <= j <= i)%nat]].
+rewrite [X in set_join X](_ : _ = a ^ i.+1 |` B'); last first.
+{ rewrite predeqP => x; split.
+  - move=> -[y [[j Hj] _ <-] <-].
+    rewrite expS /= -!expS.
+    move: Hj; rewrite leq_eqVlt ltnS => /orP[/eqP [->] | Hj]; [by left|].
+    by right; exists j.+1.
+  - move=> [->|]; [by rewrite expS; exists (a ^ i) => //; exists ord_max|].
+    move=> -[[//|j] /andP [Hj Hji] <-]; rewrite expS; exists (a ^ j) => //.
+    by move: Hji => /ltnW; rewrite -ltnS => Hji; exists (Ordinal Hji). }
+rewrite -[set_join]/set_add set_addDl.
+rewrite (_ : mkset _ = (1 |` B')); last first.
+{ rewrite predeqP => x; split.
+  { move=> -[[[|j] Hj] _ <-]; [by left; rewrite exp0|].
+    by right; exists (Ordinal Hj). }
+  move=> [-> | [j /= Hj <-]]; [by rewrite -(exp0 a); exists ord0|].
+  by move: Hj => /andP[_]; rewrite -ltnS => Hj; exists (Ordinal Hj). }
+rewrite set_addDl.
+rewrite (adddC (a ^ 0)) adddA -(adddA (a ^ i.+1)) adddd adddC -!set_addDl.
+apply: f_equal; rewrite predeqP => x; split.
+- move=> [-> | [-> | [j /= Hj <-]]].
+  + by exists ord0.
+  + by exists ord_max.
+  + move: Hj => /andP[_]; rewrite -ltnS; move=> /ltnW; rewrite -ltnS => Hj.
+    by exists (Ordinal Hj).
+- move=> -[[[|j] Hj] _ <-]; [by left|right].
+  move: Hj; rewrite /nat_of_ord !ltnS leq_eqVlt => /orP[/eqP -> | ]; [by left|].
+  by rewrite -ltnS => Hj; right; exists (Ordinal Hj).
+Qed.
+
+Lemma kleene_sqr a : a^* * a^* = a^*.
+Proof.
+rewrite {1}/op_kleene set_mulDr.
+set B' := (a ^* |` [set of fun i => a ^ i.+1 * a ^*]).
+rewrite (_ : mkset _ = B'); last first.
+{ rewrite predeqP => x; split.
+  - by move=> -[_ [[|i] _ <-] <-]; [left; rewrite exp0 mul1d|right; exists i].
+  - move=> [-> | [i _ <-]]; [by exists 1; [exists O|rewrite mul1d]|].
+    by exists (a ^ i.+1); [exists i.+1|]. }
+rewrite -[set_join]/set_add set_addDl.
+apply/eqP; rewrite adddC -le_def; apply: set_add_lim_nat.
+elim=> [|k].
+- by rewrite (set_add_0 (fun i => a ^ i.+1 * _)) le0d.
+- rewrite (set_add_S (fun i => a ^ i.+1 * _)).
+  rewrite !le_def => /eqP IHk.
+  rewrite adddAC IHk adddC -le_def.
+  exact: kleene_mul_expl.
+Qed.
+
+Lemma kleene_exp a i : (1 <= i)%nat -> (a ^*) ^ i = a ^*.
+Proof.
+case: i => [// | ] i _; elim: i => [// | i IHi].
+by rewrite expS IHi kleene_sqr.
+Qed.
+
+Lemma kleene_kleene a : (a ^*) ^* = a ^*.
+Proof.
+rewrite kleeneSr /op_plus.
+rewrite (_ : mkset _ = a ^* |` set0).
+  by rewrite setU0 /set_add set_join_set1 kleeneSr adddA adddd.
+rewrite predeqP => x; split.
+- move=> [i _ <-]; left; exact: kleene_exp.
+- by move=> [-> | //]; exists O.
+Qed.
+
+Theorem kleene_star_eq a b : a ^* * b = a * (a ^* * b) + b.
+Proof. by rewrite muldA -plusSr -{3}(mul1d b) -muldDl kleeneSr adddC. Qed.
+
+Theorem kleene_star_least a b : forall x, (x = a * x + b) -> a ^* * b <= x.
+Proof.
+move=> x Hx.
+rewrite set_mulDr.
+rewrite [X in set_join X](_ : _ = [set (a ^ i) * b | i in setT]); last first.
+  rewrite predeqP => y; split.
+  - by move=> [_ [i _ <-] <-]; exists i.
+  - by move=> [i _ <-]; exists (a ^ i); [exists i|].
+apply: set_add_lim_nat; elim=> [ | k IHk].
+  by rewrite le_def (set_add_0 (fun i => a ^ i * b)) add0d.
+rewrite (set_add_S (fun i => a ^ i * b)) led_add_eqv IHk /=.
+elim: k {IHk} => [ | k IHk]; [by rewrite mul1d Hx led_addl|].
+rewrite expS.
+have Ha : a * x <= x; [by rewrite {2}Hx led_addr|].
+by apply: (le_trans _ Ha); rewrite -muldA led_mul2l.
+Qed.
+
+End KleeneStar.
+
+Section ResiduationTheory.
+
+Lemma div_mul_le a b : (a / b) * b <= a.
+Proof. by rewrite set_mulDr; apply: set_join_le_ub => y [z + <-]. Qed.
+
+Lemma mul_div_le a b : a <= (a * b) / b.
+Proof. exact/set_join_ub/lexx. Qed.
+
+Lemma mul_div_equiv a b x : (x * a <= b) = (x <= b / a).
+Proof.
+apply/idP/idP.
+- move=> H; exact: set_join_ub.
+- move=> H.
+  move: (div_mul_le b a); apply: le_trans.
+  exact: led_mul2r.
+Qed.
+
+Lemma div_top x : top D / x = top D.
+Proof.
+apply/le_anti /andP; split; [|rewrite -mul_div_equiv]; exact: top_maximum.
+Qed.
+
+Lemma led_divl a b c : a <= b -> a / c <= b / c.
+Proof. rewrite -mul_div_equiv; exact/le_trans/div_mul_le. Qed.
+
+Lemma led_divr a b c : a <= b -> c / b <= c / a.
+Proof.
+move=> Hab; rewrite -mul_div_equiv.
+exact/(le_trans (led_mul2l  _ Hab))/div_mul_le.
+Qed.
+
+Lemma led_div a b c d : a <= b -> d <= c -> a / c <= b / d.
+Proof.
+move=> Hab Hcd; rewrite -mul_div_equiv.
+apply/(le_trans _ Hab); rewrite mul_div_equiv.
+exact: led_divr.
+Qed.
+
+Lemma addd_div_le (a b c : D) : a / c + b / c <= (a + b) / c.
+Proof.
+rewrite led_add_eqv.
+apply/andP; split; apply: led_divl; [exact: led_addr|exact: led_addl].
+Qed.
+
+Lemma div_mul a b c : a / (b * c) = a / c / b.
+Proof.
+apply/le_anti/andP; split.
+- by rewrite -!mul_div_equiv -muldA; apply: div_mul_le.
+- by rewrite -mul_div_equiv muldA !mul_div_equiv; exact: lexx.
+Qed.
+
+Lemma mul_divA a b c : a * (b / c) <= (a * b) / c.
+Proof. rewrite -mul_div_equiv -muldA; exact/led_mul2l/div_mul_le. Qed.
+
+Lemma kleene_div_equiv a : (a == a ^*) = (a == a / a).
+Proof.
+apply/idP/idP => /eqP.
+- move=> ->.
+  apply/eqP/le_anti/andP; split.
+  + apply: set_join_le_ub => _ [i _ <-].
+    rewrite -mul_div_equiv.
+    exact: kleene_mul_expl.
+  + apply: set_join_le_ub => x /=.
+    rewrite {1}kleeneSr muldDr muld1.
+    by rewrite led_add_eqv => /andP[].
+- move=> Ha; apply/eqP/le_anti/andP; split; [exact: le_kleene|].
+  apply: set_join_le_ub => _ [i _ <-].
+  elim: i => [ | i IHi]; [by rewrite exp0 Ha -mul_div_equiv mul1d|].
+  by rewrite expSr mul_div_equiv -Ha.
+Qed.
+
+Lemma mul_kleene a b : a * b ^* = (a * b ^*) / b ^*.
+Proof.
+apply/le_anti/andP; split.
+- by rewrite -mul_div_equiv -muldA kleene_sqr.
+- apply: set_join_le_ub => x.
+  apply: le_trans.
+  by rewrite kleeneSr muldDr muld1 led_addr.
+Qed.
+
+Lemma div_kleene a b : a / b ^* = (a / b ^*) * b ^*.
+Proof.
+apply/le_anti/andP; split; last first.
+- by rewrite -mul_div_equiv -muldA kleene_sqr div_mul_le.
+- apply: set_join_le_ub => x /=.
+  rewrite mul_div_equiv => Hx.
+  apply: (le_trans Hx).
+  by rewrite kleeneSr muldDr muld1 led_addr.
+Qed.
+
+End ResiduationTheory.
+
 End CompleteDioidTheory.
 
 HB.factory Record ComCompleteDioid_of_CompleteDioid D of CompleteDioid D := {
@@ -133,13 +405,13 @@ HB.end.
 HB.factory Record ComCompleteDioid_of_ComDioid_and_CompleteLattice D
            of ComDioid D & CompleteLattice D := {
   set_mulDl : forall (a : D) (B : set D),
-      a * set_join B = set_join [set a * x | x in B]%classic;
+      a * set_join B = set_join [set a * x | x in B];
 }.
 
 HB.builders Context D (f : ComCompleteDioid_of_ComDioid_and_CompleteLattice D).
 
   Lemma set_mulDr a (B : set D) :
-    set_join B * a = set_join [set x * a | x in B]%classic.
+    set_join B * a = set_join [set x * a | x in B].
   Proof.
   by rewrite muldC set_mulDl; apply: f_equal; rewrite predeqP => x; split;
     (move=> -[y Hy <-]; exists y; [|rewrite muldC]).
@@ -172,7 +444,7 @@ HB.factory Record ComCompleteDioid_of_CompleteLattice D
       (Order.POrder.le wrap_porderMixin a b)
       = (Equality.op wrap_eqMixin (add a b) b);
   set_mulDl : forall (a : D) (B : set D),
-      mul a (set_join B) = set_join [set mul a x | x in B]%classic;
+      mul a (set_join B) = set_join [set mul a x | x in B];
 }.
 
 HB.builders Context D (f : ComCompleteDioid_of_CompleteLattice D).
@@ -190,3 +462,68 @@ HB.end.
 
 HB.structure Definition ComCompleteDioid :=
   { D of CompleteDioid D & ComCompleteDioid_of_CompleteDioid D }.
+
+Section ComCompleteDioidTheory.
+
+Variables D : ComCompleteDioid.type.
+
+Implicit Types a b : D.
+
+Local Notation "a ^ i" := (@exp _ a i) : dioid_scope.
+Local Notation "a ^*" := (@op_kleene _ a) (at level 20) : dioid_scope.
+Local Notation "a ^+" := (@op_plus _ a) (at level 20) : dioid_scope.
+Local Notation "a / b" := (div a b) : dioid_scope.
+
+Section ComResiduationTheory.
+
+Lemma divAC : right_commutative (@div D).
+Proof.
+move=> a b c; apply/le_anti/andP; split; rewrite -!mul_div_equiv.
+- by rewrite -muldA (muldC b) muldA !mul_div_equiv.
+- by rewrite -muldA (muldC c) muldA !mul_div_equiv.
+Qed.
+
+Lemma kleene_add_mul a b : (a + b) ^* = a ^* * b ^*.
+Proof.
+apply/le_anti/andP; split.
+- apply: set_join_le_ub => _ [i _ <-].
+  elim: i => [ | i IHi].
+  + rewrite exp0 -(muld1 1).
+    apply: led_mul.
+    * rewrite kleeneSr -{1}(addd0 1).
+      exact/led_add2l/le0d.
+    * rewrite kleeneSr -{1}(addd0 1).
+      exact/led_add2l/le0d.
+  + rewrite expS.
+    apply: (le_trans (led_mul2l _ IHi)).
+    rewrite muldDl muldA -plusSr (muldC b) -muldA -(muldC b) -plusSr.
+    rewrite !kleeneSr !muldDl !muldDr !muld1 !mul1d.
+    rewrite !adddA (adddC (a^+)) adddC !adddA adddd.
+    rewrite [X in _ <= X]adddC -!adddA (adddC 1) (adddC (a^+)) !adddA.
+    exact: led_addr.
+- rewrite {2}/op_kleene /set_add set_mulDl.
+  apply: set_join_le_ub => _ [_ [i _ <-] <-].
+  rewrite {1}/op_kleene /set_add set_mulDr.
+  apply: set_join_le_ub => _ [_ [j _ <-] <-].
+  elim: i => [ | i HIi].
+  + rewrite exp0 muld1.
+    elim: j => [ | j HIj]; [by rewrite exp0 kleeneSr led_addr|].
+    rewrite expS kleeneSr plusSr -(add0d (_ * _)).
+    apply: led_add; [exact: le0d|].
+    apply: led_mul => [ | //].
+    exact: led_addr.
+  + rewrite expS (muldC b) muldA.
+    apply: (le_trans (led_mul2r _ HIi)).
+    rewrite muldC {2}kleeneSr plusSr -(add0d (_ * _)).
+    apply: led_add; [exact: le0d|].
+    exact/led_mul2r/led_addl.
+Qed.
+
+End ComResiduationTheory.
+
+End ComCompleteDioidTheory.
+
+Notation "a ^ i" := (@exp _ a i) : dioid_scope.
+Notation "a ^*" := (@op_kleene _ a) (at level 20) : dioid_scope.
+Notation "a ^+" := (@op_plus _ a) (at level 20) : dioid_scope.
+Notation "a / b" := (div a b) : dioid_scope.
